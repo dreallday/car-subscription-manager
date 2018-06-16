ba5864198b9c0e6cd00e7b38f6aed1d12f1e2e26
Andrei Fedianov <andreifaa@gmail.com>
2018-06-16 19:04:12 -0400

    export constants

---
+++ client/src/Constants.js
@@ -0,0 +1,12 @@
+/**
+* @providesModule Constants
+* @flow
+*/
+
+
+export const API = "http://localhost";
+export const VEHICLES = ":3003/vehicles";
+export const PRICING = ":3001";
+export const SUBSCRIPTION = ":3002";
+
+// export const 
