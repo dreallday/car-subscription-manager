ea548bd743a2750eef574294c30eb04a592e8a97
Andrei Fedianov <andreifaa@gmail.com>
2018-06-16 19:04:20 -0400

    helper method for API access

---
+++ client/src/Helpers.js
@@ -0,0 +1,38 @@
+/**
+* @providesModule Helpers
+* @flow
+*/
+
+ import axios from 'axios';
+
+ import { API, VEHICLES, PRICING, SUBSCRIPTION } from "./Constants";
+
+
+export const getAvailableVehicles = () => {
+    const url = API + VEHICLES;
+    return getAPI(url)
+}
+
+
+export const getPrice = (params) => {
+    let keys = Object.keys(params);
+    let url = API + PRICING;
+    if(keys.length > 0) {
+        url += "?"
+    }
+    keys.map(key => 
+        url += `${key}=${params[key]}&`
+    )
+    console.log("getPrice", params, url);
+    return getAPI(url)
+}
+
+const getAPI = (url) => {
+    return axios.get(url)
+        .then(data => data.data) //parsing out data from axios response
+        .then(data => data)
+        .catch((err) => {
+            console.log("getAPI error", err);
+            return [];
+        });
+}
