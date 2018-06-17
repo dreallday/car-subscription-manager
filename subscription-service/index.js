98bafec1867997e3d3a8ac278a3bb22d9197a187
Andrei Fedianov <andreifaa@gmail.com>
2018-06-16 20:38:12 -0400

    subscription server cors

--- subscription-service/index.js
+++ subscription-service/index.js
@@ -1,10 +1,25 @@
 const { send, json } = require('micro')
-const { router, post } = require('microrouter')
+const { router, post, get, options } = require('microrouter')
+
+
+let subscriptions = {};
 
 module.exports = router(
+  options('/', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    send(res, 200);
+  }),
   post('/', async (req, res) => {
+    
+
     const subscription = await json(req)
     // TODO: "Save" subscription
     // TODO: Return response
-  })
+    console.log("subscription", subscription);
+
+    send(res, 200, { confirmation: "done did it" })
+
+  }),
 )
